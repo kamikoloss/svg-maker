d402cb393693486de2ab48b4d5d96c71cfc96168
Kamiko Loss <info@gloxi.net>
2025-04-05 01:10:18 +0900

    until を date にした, パラメーターで until と since を受け付けるようにした

--- src/index.ts
+++ src/index.ts
@@ -19,19 +19,33 @@ app.get('/', (c) => {
 app.get('/color', (c) => {
   const rgb = c.req.queries('rgb');
   if (!rgb) return invalidImageResponse();
-  const html = `<div style="display: flex; height: 100vh; width: 100vw; background: #${rgb[0]};"></div>`;
+  const html = /*html*/`
+    <div style="display: flex; height: 100vh; width: 100vw; background: #${rgb[0]};"></div>
+  `;
   return new ImageResponse(html, {width: 24, height: 24});
 });
 
-app.get('/until', (c) => {
-  const date = c.req.queries('date');
-  if (!date) return invalidImageResponse();
-  const dayCount = Math.floor((Date.parse(date[0]) - Date.now()) / (1000 * 60 * 60 * 24));
+app.get('/date', (c) => {
+  const until = c.req.queries('until');
+  const since = c.req.queries('since');
+  if (!until && !since) return invalidImageResponse();
+  let date = until?.[0] || since?.[0];
+  let dayCount = 0;
+  let dayWord = '';
+  if (until) {
+    dayCount = Math.floor((Date.parse(until[0]) - Date.now()) / (1000 * 60 * 60 * 24));
+    dayWord = 'until';
+  } else if (since) {
+    dayCount = Math.floor((Date.now() - Date.parse(since[0])) / (1000 * 60 * 60 * 24));
+    dayWord = 'since';
+  } else {
+    return invalidImageResponse();
+  }
   const html = /*html*/`
     <div style="${rootDivStyle} background: #000000; color: #FFFFFF;">
       <div style="display: flex; flex-direction: column; align-items: center; gap: -16px;">
         <p style="font-size: 128px;">${dayCount}</p>
-        <p style="font-size: 32px;">days until</p>
+        <p style="font-size: 32px;">days ${dayWord}</p>
         <p style="font-size: 32px;">${date}</p>
       </div>
     </div>
