bfe1d3dc87b914071b2d99cd7ff061f7b5fe046d
Kamiko Loss <info@gloxi.net>
2025-04-04 23:48:37 +0900

    top route に workers-og の基本レスポンスを追加した

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { Hono } from 'hono'
+import { ImageResponse } from 'workers-og';
 
-const app = new Hono()
+const app = new Hono();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return invalidImageResponse();
+});
 
-export default app
+const invalidImageResponse = (): ImageResponse => {
+  const html = `
+    <div style="display: flex; align-items: center; justify-content: center; height: 100vh; width: 100vw; font-family: sans-serif; background: #000000">
+      <div style="display: flex; font-size: 256px; margin: 0; font-weight: 700; color: #FFFFFF">?</div>
+    </div>
+ `;
+  return new ImageResponse(html, {width: 320, height: 320});
+}
+
+export default app;
