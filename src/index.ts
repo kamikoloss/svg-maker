a775cdeef0d6438a8c96b139754455a2e3d22e9c
Kamiko Loss <info@gloxi.net>
2025-04-05 00:49:54 +0900

    until エンドポイントを追加した

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,15 @@ import { ImageResponse } from 'workers-og';
 
 const app = new Hono();
 
+const rootDivStyle = `
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  font-family: sans-serif;
+`;
+
 app.get('/', (c) => {
   return invalidImageResponse();
 });
@@ -14,12 +23,28 @@ app.get('/color', (c) => {
   return new ImageResponse(html, {width: 24, height: 24});
 });
 
-const invalidImageResponse = (): ImageResponse => {
-  const html = `
-    <div style="display: flex; align-items: center; justify-content: center; height: 100vh; width: 100vw; font-family: sans-serif; background: #000000">
-      <div style="display: flex; font-size: 256px; margin: 0; font-weight: 700; color: #FFFFFF">?</div>
+app.get('/until', (c) => {
+  const date = c.req.queries('date');
+  if (!date) return invalidImageResponse();
+  const dayCount = Math.floor((Date.parse(date[0]) - Date.now()) / (1000 * 60 * 60 * 24));
+  const html = /*html*/`
+    <div style="${rootDivStyle} background: #000000; color: #FFFFFF;">
+      <div style="display: flex; flex-direction: column; align-items: center; gap: -16px;">
+        <p style="font-size: 128px;">${dayCount}</p>
+        <p style="font-size: 32px;">days until</p>
+        <p style="font-size: 32px;">${date}</p>
+      </div>
     </div>
- `;
+  `;
+  return new ImageResponse(html, {width: 320, height: 320});
+});
+
+const invalidImageResponse = (): ImageResponse => {
+  const html = /*html*/`
+    <div style="${rootDivStyle} background: #000000; color: #FFFFFF;">
+      <p style="font-size: 32px;">?</p>
+    </div>
+  `;
   return new ImageResponse(html, {width: 64, height: 64});
 }
 
