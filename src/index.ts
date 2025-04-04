e554c7f8588a9722ec2c26f412726a5dfbd76485
Kamiko Loss <info@gloxi.net>
2025-04-04 23:58:25 +0900

    color エンドポイントを追加した

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,13 @@ app.get('/', (c) => {
   return invalidImageResponse();
 });
 
+app.get('/color', (c) => {
+  const rbg = c.req.queries('rbg');
+  if (!rbg) return invalidImageResponse();
+  const html = `<div style="display:flex; height: 100vh; width: 100vw; background: #${rbg[0]}"></div>`;
+  return new ImageResponse(html, {width: 32, height: 32});
+});
+
 const invalidImageResponse = (): ImageResponse => {
   const html = `
     <div style="display: flex; align-items: center; justify-content: center; height: 100vh; width: 100vw; font-family: sans-serif; background: #000000">
