9dbee59b95a6af1881c039d510ea9e620757bd90
Kamiko Loss <info@gloxi.net>
2025-04-06 02:27:36 +0900

    文字のマージンを修正した

--- src/index.ts
+++ src/index.ts
@@ -39,11 +39,11 @@ app.get('/date', async (c) => {
   }
 
   const html = /*html*/`
-    <div style="${rootDivStyle} background: #000000; color: #FFFFFF;">
-      <div style="display: flex; flex-direction: column; align-items: center; gap: -16px;">
-        <p style="font-size: ${dayFontSizePx}px;">${dayCount}</p>
-        <p style="font-size: 32px;">days ${dayWord}</p>
-        <p style="font-size: 32px;">${date[0]}</p>
+    <div style="${rootDivStyle} background: #000000; color: #FFFFFF; line-height: 1em;">
+      <div style="display: flex; flex-direction: column; align-items: center;">
+        <p style="margin: 0; font-size: ${dayFontSizePx}px;">${dayCount}</p>
+        <p style="margin: 0; font-size: 32px;">days ${dayWord}</p>
+        <p style="margin: 0; font-size: 32px;">${date[0]}</p>
       </div>
     </div>
   `;
@@ -63,10 +63,10 @@ app.get('/random', async (c) => {
   const random = Math.floor(Math.random() * randomMax) + 1;
 
   const html = /*html*/`
-    <div style="${rootDivStyle} background: #000000; color: #FFFFFF;">
-      <div style="display: flex; flex-direction: column; align-items: center; gap: -16px;">
-        <p style="font-size: 32px;">1d${randomMax}=</p>
-        <p style="font-size: 128px;">${random}</p>
+    <div style="${rootDivStyle} background: #000000; color: #FFFFFF; line-height: 1em;">
+      <div style="display: flex; flex-direction: column; align-items: center;">
+        <p style="margin: 0; font-size: 32px;">1d${randomMax}=</p>
+        <p style="margin: 0; font-size: 128px;">${random}</p>
       </div>
     </div>
   `;
@@ -90,14 +90,14 @@ app.get('/dlsite', async (c) => {
   const { dl_count, wishlist_count, work_image } = JSON.parse(json)[id[0]];
 
   const html = /*html*/`
-    <div style="${rootDivStyle} align-items: flex-end; justify-content: flex-start; color: #FFFFFF;">
+    <div style="${rootDivStyle} align-items: flex-end; justify-content: flex-start; color: #FFFFFF; line-height: 0.9em;">
       <img src="https:${work_image}" style="width: 100%; height: 100%; object-fit: cover;">
       <div style="position: absolute; display: flex; width: 100%; height: 100%; background-image: linear-gradient(to bottom, rgba(0,0,0,0.1), rgba(0,0,0,0.9));"></div>
-      <div style="position: absolute; display: flex; flex-direction: column; align-items: flex-start; gap: -24px;">
-        <p style="font-size: 32px;">DL:</p>
-        <p style="font-size: 64px;">${dl_count}</p>
-        <p style="font-size: 32px;">WL:</p>
-        <p style="font-size: 64px;">${wishlist_count}</p>
+      <div style="position: absolute; display: flex; flex-direction: column; align-items: flex-start;">
+        <p style="margin: 0; font-size: 32px;">DL:</p>
+        <p style="margin: 0; font-size: 64px;">${dl_count}</p>
+        <p style="margin: 0; font-size: 32px;">WL:</p>
+        <p style="margin: 0; font-size: 64px;">${wishlist_count}</p>
       </div>
     </div>
   `;
@@ -116,21 +116,22 @@ app.get('/steam', async (c) => {
   const id = c.req.queries('id');
   if (!id) return invalidImageResponse();
 
-  const appUrl = `https://store.steampowered.com/api/appdetails?l=ja&appids=${id[0]}`;
+  const appUrl = `https://store.steampowered.com/api/appdetails?l=en&appids=${id[0]}`;
   const appJson = await fetch(appUrl).then(res => res.text());
-  const { header_image, recommendations } = JSON.parse(appJson)[id[0]].data;
-  const reviewUrl = `https://store.steampowered.com/appreviews/${id[0]}?l=ja&json=1`;
+  const { header_image } = JSON.parse(appJson)[id[0]].data;
+  const reviewUrl = `https://store.steampowered.com/appreviews/${id[0]}?json=1&language=all`;
   const reviewJson = await fetch(reviewUrl).then(res => res.text());
   const { query_summary } = JSON.parse(reviewJson);
 
   const html = /*html*/`
-    <div style="${rootDivStyle} align-items: flex-end; justify-content: flex-start; color: #FFFFFF;">
+    <div style="${rootDivStyle} align-items: flex-end; justify-content: flex-start; color: #FFFFFF; line-height: 0.9em;">
       <img src="${header_image}" style="width: 100%; height: 100%; object-fit: cover;">
       <div style="position: absolute; display: flex; width: 100%; height: 100%; background-image: linear-gradient(to bottom, rgba(0,0,0,0.1), rgba(0,0,0,0.9));"></div>
-      <div style="position: absolute; display: flex; flex-direction: column; align-items: flex-start; gap: -24px;">
-        <p style="font-size: 32px;">RV:</p>
-        <p style="font-size: 64px;">${recommendations.total}</p>
-        <p style="font-size: 16px; padding-bottom: 16px;">${query_summary.review_score_desc}</p>
+      <div style="position: absolute; display: flex; flex-direction: column; align-items: flex-start;">
+        <p style="margin: 0; font-size: 32px;">RV:</p>
+        <p style="margin: 0; font-size: 64px;">${query_summary.total_reviews}</p>
+        <p style="margin: 0; font-size: 16px;">${query_summary.total_positive}-${query_summary.total_negative}</p>
+        <p style="margin: 0; font-size: 16px;">${query_summary.review_score_desc}</p>
       </div>
     </div>
   `;
