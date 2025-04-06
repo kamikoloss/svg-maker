2249afc7df4c6087995414c5e7195cfdd5d0b9c4
Kamiko Loss <info@gloxi.net>
2025-04-06 16:21:24 +0900

    steam の文字を修正した

--- src/steam.ts
+++ src/steam.ts
@@ -12,6 +12,13 @@ export async function getSteamImageResponse(c: Context): Promise<ImageResponse> 
   const reviewUrl = `https://store.steampowered.com/appreviews/${id[0]}?json=1&language=all`;
   const reviewJson = await fetch(reviewUrl).then(res => res.text());
   const { query_summary } = JSON.parse(reviewJson);
+  
+  let descFontColor = '#fde047'; // yellow-300
+  if (6 <= query_summary.review_score) {
+    descFontColor = '#86efac'; // green-300
+  } else if (query_summary.review_score <= 4) {
+    descFontColor = '#fca5a5'; // red-300  
+  }
 
   const html = /*html*/`
     <div style="${rootDivDefaultStyle} align-items: flex-end; justify-content: flex-start; color: #FFFFFF; line-height: 0.9em;">
@@ -20,8 +27,7 @@ export async function getSteamImageResponse(c: Context): Promise<ImageResponse> 
       <div style="position: absolute; display: flex; flex-direction: column; align-items: flex-start;">
         <p style="margin: 0; font-size: 32px;">RV:</p>
         <p style="margin: 0; font-size: 64px;">${query_summary.total_reviews}</p>
-        <p style="margin: 0; font-size: 16px;">${query_summary.total_positive}-${query_summary.total_negative}</p>
-        <p style="margin: 0; font-size: 16px;">${query_summary.review_score_desc}</p>
+        <p style="margin: 0; font-size: 32px; color: ${descFontColor};">${query_summary.review_score_desc.replace('Overwhelmingly', 'OWM')}</p>
       </div>
     </div>
   `;
